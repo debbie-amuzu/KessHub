922e704244cc4e8f813d2d3520fc1acda29195af
Deborah Amuzu <debamuzudeb@gmail.com>
2025-02-10 15:41:57 +0000

    cccommiit

--- src/page/cars.js
+++ src/page/cars.js
@@ -1,51 +1,61 @@
+import toyota from '../assets/images/toyota.jpg'
+import hondai from '../assets/images/hondai.jpg'
+import ferrari from '../assets/images/ferrari.jpg'
+import mercedes from '../assets/images/mercedes.jpg'
+import bmw from '../assets/images/bmw.jpg'
+import tesla from '../assets/images/black tesla.jpg'
+import nissan from '../assets/images/nissan.jpg'
+
+
+
 const cars = [
     {
         id: 1,
         name: "Toyota",
         price: "30,000",
-        image: "/src/assets/images/toyota.jpg",
+        image: toyota,
         description: "Reliable family sedan with excellent fuel economy"
     },
     {
         id: 2,
         name: "Honda ",
         price: "25,000",
-        image: "/src/assets/images/hondai.jpg",
+        image: hondai,
         description: "Compact car with great performance"
     },
     {
         id: 3,
         name: "Ferrari",
         price: "25,000",
-        image: "/src/assets/images/ferrari.jpg",
+        image: ferrari,
         description: "Compact car with great performance"
     },
     {
         id: 4,
         name: "Mercedes ",
         price: "25,000",
-        image: "/src/assets/images/mercedes.jpg",
+        image: mercedes,
         description: "Compact car with great performance"
     },
     {
         id: 5,
         name: "BMW",
         price: "25,000",
-        image: "/src/assets/images/bmw.jpg",
+        image: bmw,
         description: "Compact car with great performance"
     },
     {
         id: 6,
         name: "Tesla Model ",
         price: "45,000",
-        image: "/src/assets/images/black tesla.jpg",
+        image: tesla,
         description: "Electric vehicle with cutting-edge technology"
     },
     {
         id: 7,
         name: "Nissan",
         price: "45,000",
-        image: "/src/assets/images/nissan.jpg",
+        image: nissan,
        
         description: "Electric vehicle with cutting-edge technology"
     }
